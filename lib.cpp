8eb9109173b33c91a41e9809feca0f60f04b3080
andreaamaccio <115472186+andreaamaccio@users.noreply.github.com>
2022-12-12 17:50:02 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include "lib.h"
+using namespace std;
+
+bool funzione(char lista[10][20], int &posizione, char nome[]){
+    for(int i=0;i<10;i++){
+        for(int j=0;j<20;j++){
+            if(lista[i][j] !=nome[j])
+                j = 20;
+            else{
+                if(j==19){
+                    posizione = i;
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
 
