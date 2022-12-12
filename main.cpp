e5828a31a59c00cee145746304f36bf1e71a4284
andreaamaccio <115472186+andreaamaccio@users.noreply.github.com>
2022-12-12 17:49:19 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
+using namespace std;
 
 int main(){
-  return 0;
+    char lista[10][20];
+    char nome[20];
+    int posizione=0;
+
+    for(int i=0;i<10;i++){
+        for(int j=0;j<20;j++) {
+            nome[j]= ' ';
+            lista[i][j]= ' ';
+        }
+    }
+
+    for(int i=0;i<10;i++){
+        cin>>lista[i];
+    }
+    cin>>nome;
+    if(funzione(lista,posizione,nome)){
+        cout<<posizione;
+    }else{
+        cout<<"non presente";
+    }
+    return 0;
 }
